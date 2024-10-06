2b274561ee169b493ddcfea201278ebd048e35bf
dhwoo <wooda01@naver.com>
2024-10-06 23:29:20 +0900

    Order 도메인 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -55,4 +55,45 @@ public class Order {
         delivery.setOrder(this);
     }
 
+    //==생성 메서드==//
+    // 복잡한 비즈니스 로직을 하나의 생성 메서드에서 set set set으로 완결을 시킨다.
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비스니스 로직==//
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if (delivery.getStatus() == DeliveryStatus.COMP){
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+        /*
+        for (OrderItem orderItem : orderItems){
+            totalPrice += orderItem.getTotalPrice()
+            }
+        */
+        return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+    }
 }
